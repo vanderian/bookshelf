55ed4c81e73cec44fb4ed12441c70a3e711fdbaf
Marian Vanderka <vanderka.marian@gmail.com>
2020-03-15 14:20:42 +0100

    init with fastify

---
+++ src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+import { Observable, of } from 'rxjs';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {
+  }
+
+  @Get()
+  getHello(): Observable<string> {
+    return of(this.appService.getHello());
+  }
+}
